20e0e100a8779061bde5f0ffb5d7d3a3d17cb3d3
Serena Gibbons <serenalgibbons@gmail.com>
2024-03-04 21:06:41 -0500

    Add HMACs and encrypted message length

--- encryptedim.py
+++ encryptedim.py
@@ -51,44 +51,129 @@ class InstantMessenger():
             s.close()
 
     def sha256Key(k):
-        return SHA256.new(k.encode('utf-8')).hexdigest()[:16]
+        return (SHA256.new(k.encode('utf-8')).hexdigest()[:16]).encode('utf-8')
 
     def pad(s):
-        return s + (16 - len(s)%16) * chr(16 - len(s)%16)
+        return (s + (16 - len(s)%16) * chr(16 - len(s)%16)).encode('utf-8')
 
     def unpad(s):
+        s = s.decode('utf-8')
         return s[:-ord(s[-1])]
+
+    def getBlockSize(n):
+        blockSize = 16
+        while (n > blockSize):
+            blockSize += 16
+        return blockSize
 
     def encrypt(data, k1, k2):
         # create aes and hmac keys
-        aes_key = InstantMessenger.sha256Key(k1).encode('utf-8')
+        aes_key = InstantMessenger.sha256Key(k1)
+        hmac_key = InstantMessenger.sha256Key(k2)
 
         # pad data to 16 bytes
-        paddedData = (InstantMessenger.pad(data)).encode('utf-8')
-
+        paddedData = (InstantMessenger.pad(data))
+        paddedLength = (InstantMessenger.pad(str(len(data))))
+        
         # create random initialization vector
         iv = get_random_bytes(16)
 
-        # create aes cipher
+        # create AES cipher
         cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+
+        # Ek1(len(m))
+        ciphertextlength = cipher.encrypt(paddedLength)
 
         # Ek1(m)
         ciphertext = cipher.encrypt(paddedData)
+
+        # HMACk2(iv + Ek1(len(m)))
+        hmacIvLen = HMAC.new(hmac_key, msg=(iv + ciphertextlength), digestmod=SHA256).digest()
+
+        # HMACk2(Ek1(m))
+        hmacM = HMAC.new(hmac_key, msg=ciphertext, digestmod=SHA256).digest()
+
+        '''
+        print(b'iv: ' + iv)
+        print('iv length: ' + str(len(iv)))
         
-        return iv + ciphertext
+        print(b'length encrypted: ' + ciphertextlength)
+        print('length encrypted length: ' + str(len(ciphertextlength)))
+        
+        print(b'hmacIvLen: ' + hmacIvLen)
+        print('hmacIvLen length: ' + str(len(hmacIvLen)))
+
+        print(b'ciphertext: ' + ciphertext)
+        print("ciphertext length: " + str(len(ciphertext)))
+
+        print(b'hmacM: ' + hmacM)
+        print('hmacM length: ' + str(len(hmacM)))
+        '''
+        
+        # iv + Ek1(len(m)) + HMACk2(iv + Ek1(len(m))) + Ek1(m) + HMACk2(Ek1(m))
+        return iv + ciphertextlength + hmacIvLen + ciphertext + hmacM
 
     def decrypt(data, k1, k2):       
-        # create aes key
-        aes_key = InstantMessenger.sha256Key(k1).encode('utf-8')
+        # create aes and hmac keys
+        aes_key = InstantMessenger.sha256Key(k1)
+        hmac_key = InstantMessenger.sha256Key(k2)
 
-        # get IV and ciphertext from data
+        # get IV, Ek1(len(m)), and HMACk2(iv + Ek1(len(m))) from data
         iv = data[:16]
-        ciphertext = data[16:]
+        ciphertextlength = data[16:32]
+        hmacIvLen = data[32:64]
+
+        '''
+        print(b'iv: ' + iv)
+        print('iv length: ' + str(len(iv)))
         
+        print(b'length encrypted: ' + ciphertextlength)
+        print('length encrypted length: ' + str(len(ciphertextlength)))
+        
+        print(b'hmacIvLen: ' + hmacIvLen)
+        print('hmacIvLen length: ' + str(len(hmacIvLen)))
+        '''
+        
+        try:
+            # verify HMACk2(iv + Ek1(len(m)))
+            hmac = HMAC.new(hmac_key, digestmod=SHA256)
+            hmac.update(iv + ciphertextlength).verify(hmacIvLen)
+        except ValueError:
+            sys.stdout.write("ERROR: HMAC verification failed")
+            sys.exit(1)
+
+        # create AES cipher
         cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-        plaintext = cipher.decrypt(ciphertext)
+
+        # decrypt Ek1(len(m))
+        length = InstantMessenger.unpad(cipher.decrypt(ciphertextlength))
+        ciphertextlength = InstantMessenger.getBlockSize(int(length))
+        cEndIndex = 64 + ciphertextlength
+
+        # get Ek1(m) and HMACk2(Ek1(m)) from data
+        ciphertext = data[64:cEndIndex]
+        hmacM = data[cEndIndex:cEndIndex+32]
+
+        '''
+        print(b'ciphertext: ' + ciphertext)
+        print("ciphertext length: " + str(len(ciphertext)))
+
+        print(b'hmacM: ' + hmacM)
+        print('hmacM length: ' + str(len(hmacM)))
+        '''
         
-        return InstantMessenger.unpad(plaintext.decode('utf-8'))
+        try:
+            # verify HMACk2(Ek1(m))
+            hmac = HMAC.new(hmac_key, digestmod=SHA256)
+            hmac.update(ciphertext).verify(hmacM)
+        except ValueError:
+            sys.stdout.write("ERROR: HMAC verification failed")
+            sys.exit(1)
+
+        # decrypt Ek1(m)
+        plaintext = InstantMessenger.unpad(cipher.decrypt(ciphertext))
+        
+        return plaintext
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
